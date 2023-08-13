b53929bd35818d29054c534787c5fdcf5ab0ebf5
GUSTAVO ARAUJO DE SOUZA <gustavo.asouza18@senacsp.edu.br>
2023-08-13 17:38:26 -0300

    js

--- js/index.js
+++ js/index.js
@@ -1,36 +1,36 @@
-function mudar1(){
-    let img = document.getElementById("pasta1")
-    console.log("Funcionando")
-    img.src="imagens/pastaf.png"
+// function mudar1(){
+//     let img = document.getElementById("pasta1")
+//     console.log("Funcionando")
+//     img.src="imagens/pastaf.png"
    
-}
-function normal1(){
-    let img = document.getElementById("pasta1")
-    console.log("Funcionando")
-    img.src="imagens/pasta.png"
-}
-function mudar2(){
-    let img = document.getElementById("pasta2")
-    console.log("Funcionando")
-    img.src="imagens/pastaf.png"
+// }
+// function normal1(){
+//     let img = document.getElementById("pasta1")
+//     console.log("Funcionando")
+//     img.src="imagens/pasta.png"
+// }
+// function mudar2(){
+//     let img = document.getElementById("pasta2")
+//     console.log("Funcionando")
+//     img.src="imagens/pastaf.png"
    
-}
-function normal2(){
-    let img = document.getElementById("pasta2")
-    console.log("Funcionando")
-    img.src="imagens/pasta.png"
-}
-function mudar3(){
-    let img = document.getElementById("pasta3")
-    console.log("Funcionando")
-    img.src="imagens/pastaf.png"
+// }
+// function normal2(){
+//     let img = document.getElementById("pasta2")
+//     console.log("Funcionando")
+//     img.src="imagens/pasta.png"
+// }
+// function mudar3(){
+//     let img = document.getElementById("pasta3")
+//     console.log("Funcionando")
+//     img.src="imagens/pastaf.png"
    
-}
-function normal3(){
-    let img = document.getElementById("pasta3")
-    console.log("Funcionando")
-    img.src="imagens/pasta.png"
-}
+// }
+// function normal3(){
+//     let img = document.getElementById("pasta3")
+//     console.log("Funcionando")
+//     img.src="imagens/pasta.png"
+// }
 const switchModal = () => {
     const modal = document.querySelector('#modal')
     const actualStyle = modal.style.display
@@ -41,16 +41,7 @@ const switchModal = () => {
         modal.style.display =  'block'
     }
 }
-const switchModal2 =() => {
-    const modal = document.querySelector('#modal2')
-    const actualStyle = modal.style.display
-    if(actualStyle == 'block'){
-        modal.style.display = 'none'
-    }
-    else{
-        modal.style.display =  'block'
-    }
-}
+
 const btn = document.querySelector('#pasta2')
 btn.addEventListener('click',switchModal)
 
@@ -60,15 +51,47 @@ window.onclick = function (event){
         switchModal()
     }
 }
-const btnCriar = document.querySelector("#btnCriar")
-btnCriar.addEventListener('click',switchModal2)
 
-window.onclick = function (event){
-    const modal = document.querySelector('#modal2')
-    if(event.target == modal){
-        switchModal2()
+// const switchModal = () => {
+//     const modal = document.querySelector('#modal')
+//     const actualStyle = modal.style.display
+//     if(actualStyle == 'block'){
+//         modal.style.display = 'none'
+//     }
+//     else{
+//         modal.style.display =  'block'
+//     }
+// }
+// const switchModal2 =() => {
+//     const modal = document.querySelector('#modal2')
+//     const actualStyle = modal.style.display
+//     if(actualStyle == 'block'){
+//         modal.style.display = 'none'
+//     }
+//     else{
+//         modal.style.display =  'block'
+//     }
+// }
+// const btn = document.querySelector('#pasta2')
+// btn.addEventListener('click',switchModal)
+
+// window.onclick = function (event){
+//     const modal = document.querySelector('#modal')
+//     if(event.target == modal){
+//         switchModal()
+//     }
+// }
+// const btnCriar = document.querySelector("#btnCriar")
+// btnCriar.addEventListener('click',switchModal2)
+
+// window.onclick = function (event){
+//     const modal = document.querySelector('#modal2')
+//     if(event.target == modal){
+//         switchModal2()
 
         
-        switchModal()
-    }
-    }
+//         switchModal()
+//     }
+//     }
+
+
