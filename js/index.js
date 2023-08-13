fc872c53dc6cb388c2ab0b42594a5f05ff12f7b9
GUSTAVO ARAUJO DE SOUZA <gustavo.asouza18@senacsp.edu.br>
2023-08-13 18:20:56 -0300

    volta com index

--- js/index.js
+++ js/index.js
@@ -1,36 +1,36 @@
-// function mudar1(){
-//     let img = document.getElementById("pasta1")
-//     console.log("Funcionando")
-//     img.src="imagens/pastaf.png"
+function mudar1(){
+    let img = document.getElementById("pasta1")
+    console.log("Funcionando")
+    img.src="imagens/pastaf.png"
    
-// }
-// function normal1(){
-//     let img = document.getElementById("pasta1")
-//     console.log("Funcionando")
-//     img.src="imagens/pasta.png"
-// }
-// function mudar2(){
-//     let img = document.getElementById("pasta2")
-//     console.log("Funcionando")
-//     img.src="imagens/pastaf.png"
+}
+function normal1(){
+    let img = document.getElementById("pasta1")
+    console.log("Funcionando")
+    img.src="imagens/pasta.png"
+}
+function mudar2(){
+    let img = document.getElementById("pasta2")
+    console.log("Funcionando")
+    img.src="imagens/pastaf.png"
    
-// }
-// function normal2(){
-//     let img = document.getElementById("pasta2")
-//     console.log("Funcionando")
-//     img.src="imagens/pasta.png"
-// }
-// function mudar3(){
-//     let img = document.getElementById("pasta3")
-//     console.log("Funcionando")
-//     img.src="imagens/pastaf.png"
+}
+function normal2(){
+    let img = document.getElementById("pasta2")
+    console.log("Funcionando")
+    img.src="imagens/pasta.png"
+}
+function mudar3(){
+    let img = document.getElementById("pasta3")
+    console.log("Funcionando")
+    img.src="imagens/pastaf.png"
    
-// }
-// function normal3(){
-//     let img = document.getElementById("pasta3")
-//     console.log("Funcionando")
-//     img.src="imagens/pasta.png"
-// }
+}
+function normal3(){
+    let img = document.getElementById("pasta3")
+    console.log("Funcionando")
+    img.src="imagens/pasta.png"
+}
 const switchModal = () => {
     const modal = document.querySelector('#modal')
     const actualStyle = modal.style.display
@@ -41,57 +41,46 @@ const switchModal = () => {
         modal.style.display =  'block'
     }
 }
-
+const switchModal2 =() => {
+        const modal = document.querySelector('#modal2')
+        const actualStyle = modal.style.display
+        if(actualStyle == 'block'){
+            modal.style.display = 'none'
+        }
+        else{
+            modal.style.display =  'block'
+        }
+    }
 const btn = document.querySelector('#pasta2')
 btn.addEventListener('click',switchModal)
 
 window.onclick = function (event){
     const modal = document.querySelector('#modal')
-    if(event.target == modal){
+    const modal2 = document.querySelector('#modal2')
+    if(event.target == modal ){
         switchModal()
+        
+    }
+    if ( event.target == modal2 ){
+        switchModal()
+        switchModal2()
     }
 }
+const btnCriar = document.querySelector("#btnCriar")
+btnCriar.addEventListener('click',switchModal2)
 
-// const switchModal = () => {
-//     const modal = document.querySelector('#modal')
-//     const actualStyle = modal.style.display
-//     if(actualStyle == 'block'){
-//         modal.style.display = 'none'
-//     }
-//     else{
-//         modal.style.display =  'block'
-//     }
-// }
-// const switchModal2 =() => {
-//     const modal = document.querySelector('#modal2')
-//     const actualStyle = modal.style.display
-//     if(actualStyle == 'block'){
-//         modal.style.display = 'none'
-//     }
-//     else{
-//         modal.style.display =  'block'
-//     }
-// }
-// const btn = document.querySelector('#pasta2')
-// btn.addEventListener('click',switchModal)
+const switchModalSobre =() => {
+    const modal = document.querySelector('#modals')
+    const actualStyle = modal.style.display
+    if(actualStyle == 'block'){
+        modal.style.display = 'none'
+    }
+    else{
+        modal.style.display =  'block'
+    }
 
-// window.onclick = function (event){
-//     const modal = document.querySelector('#modal')
-//     if(event.target == modal){
-//         switchModal()
-//     }
-// }
-// const btnCriar = document.querySelector("#btnCriar")
-// btnCriar.addEventListener('click',switchModal2)
+}
 
-// window.onclick = function (event){
-//     const modal = document.querySelector('#modal2')
-//     if(event.target == modal){
-//         switchModal2()
-
-        
-//         switchModal()
-//     }
-//     }
-
+const btnsobre = document.querySelector('#pasta3')
+btnsobre.addEventListener('click',switchModalSobre)
 
